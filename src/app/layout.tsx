a18436b7869d30e1a143ea4ac7bbe18c677c5630
shivabhattacharjee <itsshivahere@outlook.com>
2024-08-17 22:28:08 +0530

    add: scroll to top btn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={`${inter.className} flex flex-col  min-h-screen max-w-[1800px] m-auto`}>
                 <Navbar />
+                <ScrollToTop />
                 <main className=" overflow-x-hidden">{children}</main>
                 <Footer />
             </body>
--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import { ChevronUp } from "lucide-react";
 
 const ScrollToTop = () => {
-    return <div>ScrollToTop</div>;
+    const [isVisible, setIsVisible] = useState(false);
+
+    const goToBtn = () => {
+        globalThis.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    };
+
+    useEffect(() => {
+        const listenToScroll = () => {
+            const hidden = 500;
+            const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+            if (winScroll > hidden) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        if (typeof globalThis !== "undefined") {
+            globalThis.addEventListener("scroll", listenToScroll);
+            return () => {
+                globalThis.removeEventListener("scroll", listenToScroll);
+            };
+        }
+    }, []);
+
+    return (
+        <>
+            {isVisible && (
+                <div className={`fixed flex text-white items-center duration-200 scale-100 bg-primary backdrop-blur-md z-10 right-2 bottom-20  rounded-full p-5  shadow-black/10 shadow-lg`}>
+                    <ChevronUp onClick={goToBtn} className="cursor-pointer" />
+                </div>
+            )}
+        </>
+    );
 };
 
 export default ScrollToTop;
