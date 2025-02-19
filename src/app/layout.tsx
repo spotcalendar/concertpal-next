338215210c433166d8833825901cf4a889561ac4
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-02-19 19:43:55 +0530

    [feat]: onboarding menu items & google sign in page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
-import { Inter } from "next/font/google";
+import { Instrument_Serif, Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ weight: "400", subsets: ["latin"], variable: "--font-inter" });
+const instrument = Instrument_Serif({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-instrument-serif",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+    <html className={`${inter.variable} ${instrument.variable}`} lang="en">
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
--- src/components/onboarding-sidebar.tsx
+++ src/components/onboarding-sidebar.tsx
@@ -1,4 +1,4 @@
-import GoogleLogo from "@/assets/google-logo";
+import GoogleBorderLogo from "@/assets/google-border-logo";
 import LogoCircle from "./ui/logo-circle";
 import SpotifyLogo from "@/assets/spotify-logo";
 import UserCircle from "@/assets/user-circle";
@@ -11,24 +11,50 @@ const OnboardingSidebar = () => {
         <h3 className="text-[#06402B] text-2xl font-semibold">ConcertPal</h3>
       </span>
 
-      <div className="flex flex-col gap-6">
-        <span className="flex items-center gap-5">
-          <GoogleLogo />
+      <div className="relative flex flex-col items-start gap-8">
+        <GoogleBorderLogo />
+        <Line top="44px" left="21px" />
+        <GoogleBorderLogo />
+        <Line top="123px" left="21px" />
+        <UserCircle />
+
+        {/* <span className="flex items-center gap-5">
+          <GoogleBorderLogo />
           <p className="font-semibold text-lg">Login With Google</p>
         </span>
 
         <span className="flex items-center gap-5">
-          <GoogleLogo />
+          <GoogleBorderLogo />
           <p className="font-semibold text-lg">Connect your Spotify</p>
         </span>
 
         <span className="flex items-center gap-5">
           <UserCircle />
-          <p className="font-semibold text-lg">Login With Google</p>
-        </span>
+          <p className="font-semibold text-lg">Add your details</p>
+        </span> */}
       </div>
     </aside>
   );
 };
 
 export default OnboardingSidebar;
+
+type LineProps = {
+  top: string;
+  left: string;
+};
+
+const Line = ({ top, left }: LineProps) => {
+  return (
+    <svg
+      style={{ position: "absolute", top, left }}
+      width="3"
+      height="34"
+      viewBox="0 0 3 34"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path opacity="0.35" d="M1.3374 0.189941V33.7954" stroke="#A5A5AB" stroke-width="2" />
+    </svg>
+  );
+};
