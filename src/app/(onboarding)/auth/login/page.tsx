524594a241d1ebeebbb5d2cf65994e9cf9fbf61c
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-02-23 17:40:14 +0530

    [update]: auth checks

--- src/app/(onboarding)/auth/login/page.tsx
+++ src/app/(onboarding)/auth/login/page.tsx
@@ -1,19 +1,38 @@
 import { auth } from "@/auth";
 import OnboardingForm from "@/components/onboarding-form";
+import { prisma } from "@/lib/db";
 import { redirect } from "next/navigation";
 
 const GoogleLoginPage = async () => {
   const session = await auth();
 
-  if (session?.user) redirect("http://localhost:3000/auth/spotify");
+  if (!session || !session.user || !session.user.email)
+    return (
+      <OnboardingForm
+        variant="google"
+        title="Log in with your Google account & never miss a Concert!"
+        description="Sign in with Google to let ConcertPal add your favorite artists’ concerts directly to your Google Calendar."
+      />
+    );
 
-  return (
-    <OnboardingForm
-      variant="google"
-      title="Log in with your Google account & never miss a Concert!"
-      description="Sign in with Google to let ConcertPal add your favorite artists’ concerts directly to your Google Calendar."
-    />
-  );
+  const userData = await prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+    select: {
+      isOnboarded: true,
+      onboardingStatus: true,
+      accounts: true,
+    },
+  });
+
+  if (userData && userData.isOnboarded) redirect("http://localhost:3000");
+
+  const isSpotifyConnected = userData?.accounts.map((account) => account.provider == "spotify")[0];
+
+  if (!isSpotifyConnected) redirect("http://localhost:3000/auth/spotify");
+
+  redirect("http://localhost:3000/auth/address");
 };
 
 export default GoogleLoginPage;
